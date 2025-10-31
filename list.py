380b451372bbfcefc3d8e9a0f349a6a1594cad69
xinshijimeishaonv <xinshijimeishaonv@outlook.com>
2025-10-31 10:35:52 +0800

    Update list.py

--- list.py
+++ list.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
         # 'https://t.me/s/another_channel_example', # 您可以添加更多起始URL
     ]
     
-    max_pages_to_crawl_per_source = 20 # 每个Telegram来源最多抓取的页数
+    max_pages_to_crawl_per_source = 5 # 每个Telegram来源最多抓取的页数
     concurrent_workers = 15 # 并发测试URL连通性的工作线程数
 
     main(start_urls_list, max_pages_to_crawl_per_source, concurrent_workers)
